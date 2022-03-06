6cd56eb243a82d1e2bd61abc16414cb20dc330c5
Nathan Arrowsmith <arrowsmith.nathan@student.greenriver.edu>
2022-03-05 21:20:07 -0500

    updated all routings to controller

--- controller/controller.php
+++ controller/controller.php
@@ -19,272 +19,40 @@ class Controller
 
     function products()
     {
-        //Initialize input variables
-        $fname = "";
-        $lname = "";
-        $age = "";
-        $coat = "";
-        $number = "";
-
-        //if the form has been posted
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-        {
-
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $age = $_POST['age'];
-            $coat = $_POST['coat'];
-            $number = $_POST['number'];
-
-            //If premium membership was selected
-            if (isset($_POST['premium'])) {
-                //instantiate PremiumMember object
-                $_SESSION['member'] = new PremiumMember();
-            }
-            else{
-                //instantiate member object
-                $_SESSION['member'] = new Member();
-            }
-
-
-            //Validate the data
-            if(Validator::validName($fname)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setFirstName($fname);
-            }
-            else {
-
-                //Set an error
-                $this->_f3->set('errors["fname"]', 'Please enter a valid First Name');
-            }
-
-            //Validate the data
-            if(Validator::validName($lname)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setLastName($lname);
-            }
-            else {
-
-                //Set an error
-                $this->_f3->set('errors["lname"]', 'Please enter a valid Last Name');
-            }
-
-            //Validate the data
-            if(Validator::validAge($age)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setAge($age);
-            }
-            else {
-
-                //Set an error
-                $this->_f3->set('errors["age"]', 'Please enter a valid Age');
-            }
-
-            if(Validator::validCoat($coat)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setCoat($coat);
-            }
-
-            //Validate the data
-            if(Validator::validPhone($number)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setPhone($number);
-            }
-            else {
-
-                //Set an error
-                $this->_f3->set('errors["number"]', 'Please enter a valid phone number');
-            }
-
-            //Redirect user to next page if there are no errors
-            if (empty($this->_f3->get('errors'))) {
-                $this->_f3->reroute('profile');
-            }
-
-        }
-
-        $this->_f3->set('fname', $fname);
-        $this->_f3->set('lname', $lname);
-        $this->_f3->set('userCoat', $coat);
-        $this->_f3->set('coats', DataLayer::getCoat());
-        $this->_f3->set('age', $age);
-        $this->_f3->set('number', $number);
 
         $view = new Template();
-
-        echo $view->render('views/personal-info.html');
+        echo $view->render('views/products.html');
     }
 
     function cart()
     {
-        //Initialize input variables
-        $email = "";
-        $seekCoat = "";
-        $biography = "";
-        $state = "";
-
-        //if the form has been posted
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-        {
-            $email = $_POST['email'];
-
-            //add data to the session variable
-            $_SESSION['state'] = $_POST['state'];
-
-            /*maybe*/
-            $state = $_POST['state'];
-            $_SESSION['member']->setState($state);
-
-            /*$_SESSION['biography'] = $_POST['biography'];*/
-            $seekCoat = $_POST['seekCoat'];
-            $biography = $_POST['biography'];
-
-            //Validate the data
-            if(Validator::validEmail($email)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setEmail($email);
-
-            }
-            else {
-
-                //Set an error
-                $this->_f3->set('errors["email"]', 'Please enter a valid Email');
-            }
-
-            if(Validator::validCoat($seekCoat)) {
-
-                //Add the data to the session variable
-                $_SESSION['member']->setSeekCoat($seekCoat);
-
-            }
-
-            if(strlen($biography) > 1){
-
-                //Add the data to the session variable
-                $_SESSION['member']->setBio($biography);
-
-            }
-
-
-            //Redirect user to next page if there are no errors
-            //this is where we check if user is premium or not
-
-            if (empty($this->_f3->get('errors'))) {
-
-                if($_SESSION['member'] instanceof PremiumMember) {
-                    $this->_f3->reroute('interests');
-                }
-                else{
-                    $this->_f3->reroute('summary');
-                }
-            }
-
-        }
-
-        $this->_f3->set('email', $email);
-        $this->_f3->set('states', DataLayer::getStates());
-        $this->_f3->set('userCoat', $seekCoat);
-        $this->_f3->set('coats', DataLayer::getCoat());
-        $this->_f3->set('biography', $biography);
 
         $view = new Template();
-
-        echo $view->render('views/profile-info.html');
+        echo $view->render('views/cart.html');
 
     }
 
     function about()
     {
-        //Get the interests from the model and add to F3 hive
-        $this->_f3->set('in', DataLayer::getIndoor());
-        $this->_f3->set('out', DataLayer::getOutdoor());
-
-        //if the form has been posted
-        if($_SERVER['REQUEST_METHOD'] == 'POST')
-        {
-
-            //Add the data to the session variable
-            //If indoor interests were selected
-            if (isset($_POST['in'])) {
-
-                $in = $_POST['in'];
-
-                //If interests are valid
-                if (Validator::validIndoor($in)) {
-
-                    //implode joins an array into a string,
-                    /*$in = implode(", ", $_POST['in']);*/
-
-
-                    $_SESSION['member']->setInDoorInterests($in);
-
-                }
-                else {
-                    $this->_f3->set("errors['ins']", "Invalid selection");
-                }
-            }
-            else {
-
-                $in = "No indoor interests selected";
-            }
-
-            //Add the data to the session variable
-            //If outdoor interests were selected
-            if (isset($_POST['out'])) {
-
-                $out = $_POST['out'];
-
-                //If interests are valid
-                if (Validator::validOutDoor($out)) {
-
-                    /*$out = implode(", ", $_POST['out']);*/
-
-                    $_SESSION['member']->setOutdoorInterests($out);
-
-                }
-                else {
-                    $this->_f3->set("errors['outs']", "Invalid selection");
-                }
-            }
-            else {
-
-                $out = "No outdoor interests selected";
-            }
-
-            //Redirect user to summary page
-            if (empty($this->_f3->get('errors'))) {
-                $_SESSION['in'] = $in;
-                $_SESSION['out'] = $out;
-                $this->_f3->reroute('summary');
-            }
-
-        }
 
         $view = new Template();
-
-        echo $view->render('views/interests.html');
-
+        echo $view->render('views/about.html');
 
     }
 
     function checkout()
     {
 
+        $view = new Template();
+        echo $view->render('views/checkout.html');
+
     }
 
     function summary()
     {
-        //echo "<h1>My Diner</h1>";
+
 
         //TODO: Send data to the model
-        //global $dataLayer;
-        //$dataLayer->saveOrder($order);
         $GLOBALS['dataLayer']->saveOrder($_SESSION['order']);
 
         $view = new Template();
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //This is my CONTROLLER
 
 //Turn on output buffering
-/*ob_start();*/
+ob_start();
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 
 //Start the session
 session_start();
-/*var_dump($_SESSION);*/
+var_dump($_SESSION);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
@@ -22,105 +22,44 @@ $con = new Controller($f3);
 
 //Define a default route
 $f3->route('GET /', function() {
-    //echo "<h1>Pixel_Studios</h1>";
 
     $GLOBALS['con']->home();
 
-    //$view = new Template();
-    //echo $view->render('views/home.html');
 });
 
 //Define an about us route
 $f3->route('GET /aboutUs', function() {
-    //echo "<h1>About Us</h1>";
 
-    $view = new Template();
-    echo $view->render('views/about.html');
+    $GLOBALS['con']->about();
 });
 
 //Define an product route
-$f3->route('GET /products', function() {
-    //echo "<h1>Products</h1>";
+$f3->route('GET|POST /products', function($f3) {
 
-    $view = new Template();
-    echo $view->render('views/products.html');
+    $GLOBALS['con']->products();
+
 });
 
 //Define a cart route
-$f3->route('GET /shopCart', function() {
-    //echo "<h1>Shopping Cart</h1>";
+$f3->route('GET|POST /shopCart', function($f3) {
 
-    $view = new Template();
-    echo $view->render('views/cart.html');
+    $GLOBALS['con']->cart();
+
 });
 
 //Define a checkout route
-$f3->route('GET /checkout', function() {
-    //echo "<h1>Form</h1>";
+$f3->route('GET|POST /checkout', function($f3) {
 
-    $view = new Template();
-    echo $view->render('views/checkout.html');
+    $GLOBALS['con']->checkout();
+
 });
 
 
-
-
-//Define a route for order 1
-/*$f3->route('GET|POST /order1', function($f3) {
-    //echo "<h1>Order 1 Form</h1>";
-
-    //If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        //TODO: Validate the data
-
-        //Add the data to the session variable
-        $_SESSION['food'] = $_POST['food'];
-        $_SESSION['meal'] = $_POST['meal'];
-
-        //Redirect user to next page
-        $f3->reroute('order2');
-    }
-
-    $view = new Template();
-    echo $view->render('views/orderForm1.html');
-});*/
-
-//Define a route for order 2
-/*$f3->route('GET|POST /order2', function($f3) {
-    //echo "<h1>Order 1 Form</h1>";
-
-    //If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        //TODO: Validate the data
-
-        //Add the data to the session variable
-        //If condiments were selected
-        if (isset($_POST['conds'])) {
-            $_SESSION['conds'] = implode(", ", $_POST['conds']);
-        }
-        else {
-            $_SESSION['conds'] = "None selected";
-        }
-
-        //Redirect user to summary page
-        $f3->reroute('summary');
-    }
-
-    $view = new Template();
-    echo $view->render('views/orderForm2.html');
-});*/
-
 // Define a summary route
 $f3->route('GET /summary', function() {
-    //echo "<h1>My Diner</h1>";
 
-    $view = new Template();
-    echo $view->render('views/summary.html');
+    $GLOBALS['con']->summary();
 
-    //Clear the session data
-    session_destroy();
 });
 
 //Run fat-free
