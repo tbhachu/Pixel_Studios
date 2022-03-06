60d4ccffb320a0fa461474ea14c409e3c36ba97d
Nathan Arrowsmith <arrowsmith.nathan@student.greenriver.edu>
2022-03-06 17:02:03 -0500

    added uneccesary functions to data layer, i am going to replace these with database calls

--- model/data-layer.php
+++ model/data-layer.php
@@ -53,6 +53,18 @@ class DataLayer
     }
 
     /**
+     * @return array[] this will eventually be replaced with database grabs
+     */
+    static function getProductDetails()
+    {
+        return array (
+            array (self::getImages()[0], self::getTitles()[0], self::getInfo()[0]),
+            array (self::getImages()[1], self::getTitles()[1], self::getInfo()[1]),
+            array (self::getImages()[2], self::getTitles()[2], self::getInfo()[2])
+        );
+    }
+
+    /**
      * Return an array of image prices
      * @return string[]
      */
@@ -60,5 +72,30 @@ class DataLayer
     {
         return array('19.99', '29.99', '49.99','64.99');
     }
+
+    static function product_card ($link, $title, $info)
+    {
+        echo "<div class='col-sm-4'>
+                <div class='mb-3'>
+                    <div class='card-body p-2'>
+                        <img class='card-img-top' src='$link' alt=''$title'>
+                        <h3 class='productTitle mt-2'>'$title'</h3>
+                        <p class='lead productDescription'>$info</p>
+                        <p class='text-right'>
+                            <button class='btn btn-success' id='product' type='submit'>
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>";
+    }
+
+    static function output_product_card()
+    {
+        self::product_card(self::getImages()[0], self::getTitles()[0], self::getInfo()[0]);
+        self::product_card(self::getImages()[1], self::getTitles()[1], self::getInfo()[1]);
+        self::product_card(self::getImages()[2], self::getTitles()[2], self::getInfo()[2]);
+    }
+
 
 }
---
+++ views/products.php
@@ -0,0 +1,161 @@
+<include href="views/header.html"></include>
+
+
+
+    <div class="container">
+
+        <h1>Products</h1>
+        <div class="row">
+
+            <!-- use fat free to populate this page, generate from an array -->
+
+
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/dundee graffiti.jpeg" alt="Photo1">
+                        <h3 class="productTitle mt-2">"Title"</h3>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!--<div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/LONG_EXP_CAR.jpg" alt="Photo2">
+                        <h3 class="productTitle mt-2">Photo 1</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product9" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/dundee graffiti.jpeg" alt="Photo1">
+                        <h3 class="productTitle mt-2">"Title"</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product2" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/LONG_EXP_CAR.jpg" alt="Photo2">
+                        <h3 class="productTitle mt-2">Photo 1</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product3" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/dundee graffiti.jpeg" alt="Photo1">
+                        <h3 class="productTitle mt-2">"Title"</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product4" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/LONG_EXP_CAR.jpg" alt="Photo2">
+                        <h3 class="productTitle mt-2">Photo 1</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product1" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/dundee graffiti.jpeg" alt="Photo1">
+                        <h3 class="productTitle mt-2">"Title"</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product6" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/LONG_EXP_CAR.jpg" alt="Photo2">
+                        <h3 class="productTitle mt-2">Photo 1</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product7" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="mb-3">
+                    <div class="card-body p-2">
+                        <img class="card-img-top" src="images/dundee graffiti.jpeg" alt="Photo1">
+                        <h3 class="productTitle mt-2">"Title"</h3>
+                        <p class="price font-weight-bold">$19.99</p>
+                        <p class="lead productDescription">Some text about the photo..</p>
+                        <p class="text-right">
+                            <button class="btn btn-success" id="product8" type="submit">
+                                Add to Cart</button>
+                        </p>
+                    </div>
+                </div>
+            </div>-->
+
+        </div>
+
+    </div>
+
+
+
+   <!-- <a class="btn btn-info mb-3" href="{{ @BASE }}" >Home</a>
+    <a class="btn btn-info mb-3" href="aboutUs" >About Us</a>
+    -->
+</div>
+
+<include href="views/footer.html"></include>
+
