868ec14800f8b73301283918bc05e35d04b14c44
Nathan Arrowsmith <arrowsmith.nathan@student.greenriver.edu>
2022-03-09 20:18:51 -0500

    main slideshow is dynamically generated from database except for first active slide

---
+++ script/script.js
@@ -0,0 +1,16 @@
+function setActive(){
+
+    /*if(document.getElementById("1").className == "carousel-item"){
+        document.getElementById("1").className = "carousel-item active";
+    }
+    else{
+        document.getElementById("1").className = "carousel-item"
+    }
+
+
+    document.getElementsByClassName("carousel-inner").firstChild.className = "carousel-item active";*/
+
+    let active = document.getElementsByClassName("carousel-item");
+    console.log(active);
+
+}
