e8d9083223f58079075ef9e9866d8818b6c6a968
Nathan Arrowsmith <arrowsmith.nathan@student.greenriver.edu>
2022-03-12 16:23:46 -0500

    creating addToCart function to build an item object with user selections

--- controller/controller.php
+++ controller/controller.php
@@ -31,6 +31,110 @@ class Controller
         echo $view->render('views/products.html');
     }
 
+    function addToCart()
+    {
+        //Get the data from the model
+        $products = $GLOBALS['dataLayer']->getProducts();
+        $this->_f3->set('products', $products);
+
+        //Initialize input variables
+        $title = "";
+        $link = "";
+        $size = "";
+        $frame = "";
+        $finish = "";
+        $price = 49.99;
+
+        //if the form has been posted
+        if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+
+            //title and link will already be generated
+            $title = $_POST['title'];
+            $link = $_POST['link'];
+
+            $size = $_POST['size'];
+            $frame = $_POST['frame'];
+            $finish = $_POST['finish'];
+
+            //$price = $_POST['price'];
+
+            //instantiate item object
+            $_SESSION['item'] = new Item();
+
+
+            //Validate the data
+            if(Validator::validName($fname)) {
+
+                //Add the data to the session variable
+                $_SESSION['member']->setFirstName($fname);
+            }
+            else {
+
+                //Set an error
+                $this->_f3->set('errors["fname"]', 'Please enter a valid First Name');
+            }
+
+            //Validate the data
+            if(Validator::validName($lname)) {
+
+                //Add the data to the session variable
+                $_SESSION['member']->setLastName($lname);
+            }
+            else {
+
+                //Set an error
+                $this->_f3->set('errors["lname"]', 'Please enter a valid Last Name');
+            }
+
+            //Validate the data
+            if(Validator::validAge($age)) {
+
+                //Add the data to the session variable
+                $_SESSION['member']->setAge($age);
+            }
+            else {
+
+                //Set an error
+                $this->_f3->set('errors["age"]', 'Please enter a valid Age');
+            }
+
+            if(Validator::validCoat($coat)) {
+
+                //Add the data to the session variable
+                $_SESSION['member']->setCoat($coat);
+            }
+
+            //Validate the data
+            if(Validator::validPhone($number)) {
+
+                //Add the data to the session variable
+                $_SESSION['member']->setPhone($number);
+            }
+            else {
+
+                //Set an error
+                $this->_f3->set('errors["number"]', 'Please enter a valid phone number');
+            }
+
+            //Redirect user to next page if there are no errors
+            if (empty($this->_f3->get('errors'))) {
+                $this->_f3->reroute('profile');
+            }
+
+        }
+
+        $this->_f3->set('fname', $fname);
+        $this->_f3->set('lname', $lname);
+        $this->_f3->set('userCoat', $coat);
+        $this->_f3->set('coats', DataLayer::getCoat());
+        $this->_f3->set('age', $age);
+        $this->_f3->set('number', $number);
+
+        $view = new Template();
+        echo $view->render('views/products.html');
+    }
+
     function cart()
     {
 
